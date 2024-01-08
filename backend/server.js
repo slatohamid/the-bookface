96b29c23c38d12f19cd4edcf55487c0dd692a8f6
Yannick Yanga <cleps2012@gmail.com>
2024-01-08 17:46:27 +0100

    setup MongoDb

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,20 @@ const cors = require("cors");
 const app = express();
 const { readdirSync } = require("fs");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 
 dotenv.config();
 
 app.use(cors());
 
+// Routes
 readdirSync("./routes").map(route => app.use("/", require("./routes/" + route)));
+
+// Database
+mongoose.connect(process.env.DATABASE_URL)
+  .then(() => console.log("database connected successfully"))
+  .catch(error => console.log("error connecting to mongodb", error));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
