f41a6cba8354a6495884a5edd01325f0737a2789
slatohamid <hslato@gmail.com>
2024-01-13 02:08:44 +0100

    Register form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Profile from "./pages/profile";
 import Home from "./pages/home";
--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -2,8 +2,7 @@ export function userReducer(state = null, action) {
   switch (action.type) {
     case "LOGIN":
       return action.payload;
-    case "LOGOUT":
-      return null;
+
     default:
       return state;
   }
