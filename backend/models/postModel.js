bc7ccc462475744cdada59fadc457c189d0e007c
Yannick Yanga <cleps2012@gmail.com>
2024-01-15 13:44:37 +0100

    update backend

---
+++ backend/models/postModel.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "Users"
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    images: { type: String },
+    likes: [{ type: String }],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comments"
+      }
+    ],
+  },
+  { timestamps: true }
+);
+
+const Posts = mongoose.model("Posts", postSchema);
+
+export default Posts;
