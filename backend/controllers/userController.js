0b4a1744e3e5a41c2909bb30a3185f7ba79c7a3b
Yannick Yanga <cleps2012@gmail.com>
2024-01-16 17:11:14 +0100

    posts routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import EmailVerification from "../models/emailVerification.js";
 import Users from "../models/userModel.js";
+import FriendRequest from "../models/friendsRequest.js";
 import { compareString, createJWT, hashString } from "../utils/index.js";
 import PasswordReset from "../models/passwordReset.js";
 import { resetPasswordLink } from "../utils/sendEmail.js";
@@ -200,6 +201,7 @@ export const updateUser = async (req, res, next) => {
 
     if (!(firstName || lastName || location || profession)) {
       next("Please provide all required fields.");
+      return;
     }
 
     const { userId } = req.body.user;
@@ -213,7 +215,7 @@ export const updateUser = async (req, res, next) => {
       _id: userId,
     };
 
-    const user = await Users.findOneAndUpdate(userId, updateUser, {
+    const user = await Users.findByIdAndUpdate(userId, updateUser, {
       new: true,
     });
 
@@ -231,6 +233,184 @@ export const updateUser = async (req, res, next) => {
       message: "User updated successfully.",
       user,
       token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const friendRequest = async (req, res, next) => {
+  try {
+    const { userId } = req.body.user;
+
+    const { requestTo } = req.body;
+
+    const requestExist = await FriendRequest.findOne({
+      requestFrom: userId,
+      requestTo,
+    });
+
+    if (requestExist) {
+      next("Friend Request already sent.");
+      return;
+    }
+
+    const accountExist = await FriendRequest.findOne({
+      requestFrom: requestTo,
+      requestTo: userId,
+    });
+
+    if (accountExist) {
+      next("Friend Request already sent.");
+      return;
+    }
+
+    const newRes = await FriendRequest.create({
+      requestTo,
+      requestFrom: userId,
+    });
+
+    if (!newRes) {
+      next("Friend Request failed");
+      return;
+    }
+
+    res.status(201).json({
+      success: true,
+      message: "Friend Request sent successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "auth error",
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+export const getFriendRequest = async (req, res) => {
+  try {
+    const { userId } = req.body.user;
+
+    const request = await FriendRequest.find({
+      requestTo: userId,
+      requestStatus: "Pending",
+    })
+      .populate({
+        path: "requestFrom",
+        select: "firstName lastName profileUrl profession -password",
+      })
+      .limit(10)
+      .sort({
+        _id: -1,
+      });
+
+    res.status(200).json({
+      success: true,
+      data: request,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "auth error",
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+export const acceptRequest = async (req, res, next) => {
+  try {
+    const id = req.body.user.userId;
+
+    const { rid, status } = req.body;
+
+    const requestExist = await FriendRequest.findById(rid);
+
+    if (!requestExist) {
+      next("No Friend Request Found.");
+      return;
+    }
+
+    const newRes = await FriendRequest.findByIdAndUpdate(
+      { _id: rid },
+      { requestStatus: status }
+    );
+
+    if (status === "Accepted") {
+      const user = await Users.findById(id);
+
+      user.friends.push(newRes?.requestFrom);
+
+      await user.save();
+
+      const friend = await Users.findById(newRes?.requestFrom);
+
+      friend.friends.push(newRes?.requestTo);
+
+      await friend.save();
+    }
+
+    res.status(201).json({
+      success: true,
+      message: "Friend Request " + status,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "auth error",
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+export const profileViews = async (req, res, next) => {
+  try {
+    const { userId } = req.body.user;
+    const { id } = req.body;
+
+    const user = await Users.findById(id);
+
+    user.views.push(userId);
+
+    await user.save();
+
+    res.status(201).json({
+      success: true,
+      message: "Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "auth error",
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+export const suggestedFriends = async (req, res) => {
+  try {
+    const { userId } = req.body.user;
+
+    let queryObject = {};
+
+    queryObject._id = { $ne: userId };
+
+    queryObject.friends = { $nin: userId };
+
+    let queryResult = Users.find(queryObject)
+      .limit(15)
+      .select("firstName lastName profileUrl profession -password");
+
+    const suggestedFriends = await queryResult;
+
+    res.status(200).json({
+      success: true,
+      data: suggestedFriends,
     });
   } catch (error) {
     console.log(error);
